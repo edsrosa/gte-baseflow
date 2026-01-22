0217866a57e074d6eafbcb56cc805020ee08e1fc
edsrosa <ednilsonrosa@outlook.com.br>
2026-01-22 20:12:42 -0300

    refatore baseflow

--- gtebaseflow/src/baseflow.py
+++ gtebaseflow/src/baseflow.py
@@ -6,18 +6,9 @@ from tools.viewer import Fig2D
 from src import utils
 
 
-def load_about(files_up_q, row):
+def load_about(row):
     """Carrega a página sobre caso não haja arquivo carregado."""
-    if files_up_q == []:
-        row.html('gtebaseflow/src/about.html')
-
-
-def select_inline(lb, ops, key_id, index=0,  pc=0.5):
-    """Gera entrada para selecionar nome de coluna."""
-    c01, c02 = st.columns([pc, 1-pc])
-    c01.write(lb)
-    select_in = c02.selectbox(lb, options=ops, key=key_id, index=index, label_visibility='collapsed')
-    return select_in
+    row.html('gtebaseflow/src/about.html')
 
 
 def num_in_inline(lb, value, pc=0.5):
@@ -36,108 +27,15 @@ def text_in_inline(lb, value, pc=0.5):
     return txt_in
 
 
-def get_filenames(files_up):
-    """Recupera as propriedades do arquivo carregados."""
-    files_byname  = {}
-    if files_up != []:
-        for f in files_up:
-            files_byname[f.name] = f
-    return files_byname
-
-
-def get_shtnames(filename, files_byname):
-    """Recupera as abas do arquivo atual."""
-    if filename is not None:
-        file = files_byname[filename]
-        shts_name = list(pd.ExcelFile(file).sheet_names)
-    else:
-        shts_name = []
-    return shts_name
-
-
-def get_colsname(files_byname, filename, sht_name):
-    """Recupera o nome das colunas do arquivo atual."""
-    cols_name = []
-    if sht_name is not None:
-        df = pd.read_excel(files_byname[filename], sheet_name=sht_name)
-        cols_name = list(df.columns)
-    return cols_name
-
-
-def get_cols_types(df):
-    """Retorna os tipos das colunas como um dicionario."""
-    dtys = list(df.dtypes.unique())
-    dtypes_cols = {dty: [] for dty in dtys}
-    dtypes_cols['all']=[]
-    nm_dty = dict(df.dtypes)
-    for nm, dty in nm_dty.items():
-         dtypes_cols[dty].append(nm)
-         dtypes_cols['all'].append(nm)
-    return dtypes_cols
-
-
-def get_value(label, key_values, key_id, index=None):
-    """Recupera o tipo de dado"""
-    value = select_inline(lb=label, ops=key_values.keys(), key_id=key_id, index=index)
-
-    if value == None:
-        return value
-    else:
-        return key_values[value]
-
-
-def get_value_ops(label, value_in, value_ops, key_id):
-    """Recupera a a opção a partir de uma lista de opções"""
-    if value_in == None:
-        ops = []
-    else:
-        ops = value_ops[value_in]
-    op = select_inline(lb=label, ops=ops, key_id=key_id)
-    return op
-
-
-def get_num_month(label, index, key_id):
-    """Recupera mês de início do perído chuvoso e seco."""
-    months = {'Janeiro': 1,
-                'Fevereiro': 2,
-                'Março': 3,
-                'Abril': 4,
-                'Maio':5,
-                'Junho':6,
-                'Julho':7,
-                'Agosto':8,
-                'Setembro':9,
-                'Outubro': 10,
-                'Novembro': 11,
-                'Dezembro':12}
-    month = get_value(label=label, key_values=months, key_id=key_id, index=index)
-    return month
-
-
-def get_type_plu(label, index, key_id):
-    """Recupera o tipo de série histórica de precipitação."""
-    types_plu= {'Diário - Média': 1,
-                'Diário - Acumulado': 2,
-                'Semanal - Média': 3,
-                'Semanal - Acumulado': 4,
-                'Mensal - Média':5,
-                'Mensal - Acumulado':6,
-                'Anual - Média':7,
-                'Anual - Acumulado':8,
-                }
-    type_plu = get_value(label=label, key_values=types_plu, key_id=key_id, index=index)
-    return type_plu
-
-
-def load_station(files_byname, filename, sht_name, col_datetime, col_streamflow):
+def load_station(files_byname, filename, sht_name, cols_in_q):
     """Carrega a estação"""
-    if col_datetime is not None and col_streamflow is not None:
+    if cols_in_q['datetime'][1] is not None and cols_in_q['streamflow'][1] is not None:
         station = Station()
         station.set_parameters(file_obj=files_byname[filename],
                             filename=filename,
                             sht_ts=sht_name,
-                            col_datetime=col_datetime,
-                            col_streamflow=col_streamflow
+                            col_datetime=cols_in_q['datetime'][1],
+                            col_streamflow=cols_in_q['streamflow'][1]
                             )
         station.load_df()
         st.session_state['station'] = station
@@ -178,32 +76,38 @@ def plot_chart(row02):
         row02.plotly_chart(fig_q.fig)
 
 
+def load_streamflow(row01):
+    """Carregamento dos dados de vazão"""
+    cols_in_q = {'datetime': ['Data:', None], 
+                'streamflow': ['vazão (m³/s):', None]}
+    files_byname_q, filename_q, shtname_q = utils.choose_xlsx(title="Vazão", 
+                    label_up="Carregue os arquivos de vazão:",
+                    cols_in=cols_in_q,
+                    sufix_id='q',
+                    multiple=True)
+    
+    if files_byname_q != {}:
+        load_station(files_byname_q, filename_q, shtname_q, cols_in_q)
+    else:
+        load_about(row01)
+
+
+def load_rainfall():
+    """Carregamento dos dados de precipitação"""
+    cols_in_plu = {'datetime': ['Data:', None], 
+                    'rainfall': ['Precipitação (mm):', None]}
+    files_byname_plu, filename_plu, shtname_plu = utils.choose_xlsx(title="Precipitação", 
+                    label_up="Carregue o arquivo de precipitação:",
+                    cols_in=cols_in_plu,
+                    sufix_id='plu',
+                    multiple=False)
+    
+
 def input_box(row01):
     """Entrada de arquivos."""
     with st.sidebar.expander("Arquivos de Entrada", expanded=True):
-        files_up_q, files_byname_q, filename_q, shtname_q, cols_name, col_datetime, col_streamflow = utils.load_xlsx(title="Vazão", 
-                        label_up="Carregue os arquivos de vazão:",
-                        multiple=True)
-        
-        load_station(files_byname_q, filename_q, shtname_q, col_datetime, col_streamflow)
-        load_about(files_up_q, row01)
-
-        with st.expander('Precipitação'):
-            st.info('Dados de precipitação são opcionais, porém enriquecem a análise.')
-            files_up_plu = st.file_uploader('Carregue o arquivo de precipitação:', type='xlsx',  accept_multiple_files=False)
-            if files_up_plu != None:
-                files_up_plu = [files_up_plu]
-            else:
-                files_up_plu = []
-            
-            files_byname_plu = get_filenames(files_up_plu)
-            filename_plu = select_inline(lb="Arquivo:", ops=files_byname_plu.keys(), key_id='filename_plu')
-            shts_name_plu = get_shtnames(filename_plu, files_byname_plu)
-            sht_name_plu = select_inline(lb="Planilha:", ops=shts_name_plu, key_id='sht_name_plu')
-            cols_name_plu = get_colsname(files_byname_plu, filename_plu, sht_name_plu)
-            col_datetime_plu = select_inline(lb='Data:', ops=cols_name_plu, index=None, key_id='col_datetime_plu')
-            col_value_plu = select_inline(lb='Precipitação (mm):', ops=cols_name_plu, index=None, key_id='col_value_plu')
-            type_plu = get_type_plu(label="Tipo de Dados:", index=0, key_id='type_plu')
+        load_streamflow(row01)
+        load_rainfall()
 
 
 def config_box():
@@ -211,11 +115,12 @@ def config_box():
     with st.sidebar.expander("Configurações", expanded=True):
         name_station = st.text_input(label='name_station', label_visibility='collapsed', placeholder='Nome da Estação')
         area_bacia = st.number_input(label='area_bacia', label_visibility='collapsed', placeholder='Área de Bacia (km²)', min_value=0.0000001, value=None, format="%0.6f")
-        start_wet = get_num_month(label='Início Período Chuvoso:', index=9, key_id='start_wet')
-        start_dry = get_num_month(label='Início Período Seco:', index=3, key_id='start_dry')
+        start_wet = utils.get_num_month(label='Início Período Chuvoso:', index=9, key_id='start_wet')
+        start_dry = utils.get_num_month(label='Início Período Seco:', index=3, key_id='start_dry')
 
         get_configs(name_station, area_bacia, start_wet, start_dry)
         classify_season(start_wet, start_dry)
+        # type_plu = utils.get_type_plu(label="Dados Precipitação:", index=0, key_id='type_plu')
 
 
 def process_box(row02):
